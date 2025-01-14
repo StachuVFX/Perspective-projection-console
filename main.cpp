601c5f8e0d13c56031bb356986627d9d7ccc35ce
Stanisław Trela <125069097+StachuVFX@users.noreply.github.com>
2025-01-14 20:46:42 +0100

    not finished (whatever i was doing)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <Windows.h>
+#include <fstream>
+#include <string>
 
 #include "screen.h"
+
+#define MODELS_FILENAME "models.json"
+#define OBJECTS_FILENAME "objects.json"
 
 #define INTERVAL 10
 
@@ -11,6 +16,9 @@ int main()
     //  TODO:
     // - not displaying negative z (done)
     // - read objects from a file
+    //      - make example files in JSON (done)
+    //      - reading the files
+    //      - converting file content to objects
     // - make proper rotation using vectors
     // - absolute coordinates to camera coordinates
     // - threads?
@@ -20,7 +28,7 @@ int main()
     Horizontal_Line hLine(0);
     Vertical_Line vLine(0);
 
-    std::vector<Point_3D> pyramid = {
+    /*std::vector<Point_3D> pyramid = {
         Point_3D(-16,-16,-16),
         Point_3D( 16,-16,-16),
         Point_3D( 16,-16, 16),
@@ -39,12 +47,39 @@ int main()
         Line_2D_Indices(2, 4),
         Line_2D_Indices(3, 4)
     };
-    bool pyramid_ifLines = true;
+    bool pyramid_ifLines = true;*/
 
-    std::vector<Object_3D> objectVector = {
-        //Box( 32, 32, 32, Point_3D(0, 0, 40), Point_3D(0, 0, 0), true)
-        Object_3D(pyramid, pyramid_position, pyramid_rotation, pyramid_linesIndices, pyramid_ifLines)
-    };
+    //std::vector<Object_3D> objectVector = {
+    //    //Box( 32, 32, 32, Point_3D(0, 0, 40), Point_3D(0, 0, 0), true)
+    //    Object_3D(pyramid, pyramid_position, pyramid_rotation, pyramid_linesIndices, pyramid_ifLines)
+    //};
+    std::vector<Object_3D> objectVector;
+
+    //      Reading object files
+    std::ifstream objectsFile(OBJECTS_FILENAME);
+    std::ifstream modelsFile(MODELS_FILENAME);
+
+    std::string textLine;
+    while (getline(objectsFile, textLine))
+    {
+        if (textLine.find("\"objects\""))
+        {
+            while (getline(objectsFile, textLine))
+            {
+                if (textLine.find("{"))
+                {
+                    char name[30];
+                    while (getline(objectsFile, textLine))
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+
+    modelsFile.close();
+    objectsFile.close();
 
     //      Drawing:
     // screen
